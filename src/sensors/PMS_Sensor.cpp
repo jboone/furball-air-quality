d97cb27485d075f95f0080d5d22aaae8ada8cfbf
Jared Boone <jboone@earfeast.com>
2020-09-18 10:31:32 -0700

    Formatting.
    Add braces around single-line if clause. De-indent the following code, which was implying it was part of the if clause.

--- src/sensors/PMS_Sensor.cpp
+++ src/sensors/PMS_Sensor.cpp
@@ -24,49 +24,50 @@ static tick_t lastRead = millis();
 
 void PMS_Sensor::handle() {
   const auto now = millis();
-  
-  if(now - _last_read_request < PMS_READ_DELAY)
+
+  if(now - _last_read_request < PMS_READ_DELAY) {
     return;
+  }
 
-    const auto n = Pmsx003::Reserved;
-    Pmsx003::pmsData data[n];
+  const auto n = Pmsx003::Reserved;
+  Pmsx003::pmsData data[n];
 
-    Pmsx003::PmsStatus status = _pms->read(data, n);
+  Pmsx003::PmsStatus status = _pms->read(data, n);
 
-    switch (status) {
-    case Pmsx003::OK: {
+  switch (status) {
+  case Pmsx003::OK: {
 #ifdef VERBOSE
-      Serial.println("_________________");
-      auto newRead = millis();
-      Serial.print("Wait time ");
-      Serial.println(newRead - _last_read_request);
+    Serial.println("_________________");
+    auto newRead = millis();
+    Serial.print("Wait time ");
+    Serial.println(newRead - _last_read_request);
 
-      // For loop starts from 3
-      // Skip the first three data (PM1dot0CF1, PM2dot5CF1, PM10CF1)
-      for (size_t i = Pmsx003::PM1dot0; i < n; ++i) { 
-	Serial.print(data[i]);
-	Serial.print("\t");
-	Serial.print(Pmsx003::dataNames[i]);
-	Serial.print(" [");
-	Serial.print(Pmsx003::metrics[i]);
-	Serial.print("]");
-	Serial.println();
-      }
+    // For loop starts from 3
+    // Skip the first three data (PM1dot0CF1, PM2dot5CF1, PM10CF1)
+    for (size_t i = Pmsx003::PM1dot0; i < n; ++i) { 
+      Serial.print(data[i]);
+      Serial.print("\t");
+      Serial.print(Pmsx003::dataNames[i]);
+      Serial.print(" [");
+      Serial.print(Pmsx003::metrics[i]);
+      Serial.print("]");
+      Serial.println();
+    }
 
 #endif
 
-      _density_1_0 = data[0];
-      _density_2_5 = data[1];
-      _density_10_0 = data[2];
+    _density_1_0 = data[0];
+    _density_2_5 = data[1];
+    _density_10_0 = data[2];
 
-      break;
-    }
-    case Pmsx003::noData:
-      break;
-    default:
-      Serial.println("_________________");
-      Serial.println(Pmsx003::errorMsg[status]);
-    };
+    break;
+  }
+  case Pmsx003::noData:
+    break;
+  default:
+    Serial.println("_________________");
+    Serial.println(Pmsx003::errorMsg[status]);
+  };
 
-    _last_read_request = now;
+  _last_read_request = now;
 }
